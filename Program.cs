658e958b4070ed735a0e0df976e361c1071a8e54
Vladislav <valdb1205@gmail.com>
2024-03-21 20:05:37 +0300

    code V2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿Console.Clear();
+int LenAr(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string el = arr[i];
+        if (el.Length <= 3)
+            count++;
+
+    }
+    return count;
+}
+
 void FillArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -8,25 +21,27 @@ void FillArray(string[] array)
         array[i] = elem;
     }
 }
+
 void CorrectArray(string[] array, string[] Corarr)
 {
-
+    int k = 0;
     for (int j = 0; j < array.Length; j++)
     {
-        if (array[j].Length <= 3)
-            Corarr[j] = array[j];
-        if (array[j].Length > 3)
-            Corarr[j] = "";
-
+        string el = array[j];
+        if (el.Length <= 3)
+        {
+            Corarr[k] = el;
+            k++;
+        }
     }
 }
+
 void PrintArr(string[] arr)
 {
 
     for (int m = 0; m < arr.Length; m++)
     {
-        if (arr[m] != "")
-            Console.Write($"'{arr[m]}'");
+        Console.Write($"'{arr[m]}'");
     }
 
 }
@@ -35,7 +50,8 @@ void PrintArr(string[] arr)
 Console.WriteLine("Укажите размер массива: ");
 int l = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[l];
-string[] CorArray = new string [l];
 FillArray(array);
+int count = LenAr(array);
+string[] CorArray = new string [count];
 CorrectArray(array, CorArray);
 PrintArr(CorArray);
